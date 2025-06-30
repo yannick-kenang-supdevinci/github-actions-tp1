3cfea9829f8314b3a341d451cc121ac450c78e45
yannick-jeefre <107536172+yannick-jeefre@users.noreply.github.com>
2025-06-30 16:39:50 +0200

    add bug for experience

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ def predict_sentiment (text):
     if not text:
         return "neutral"
     if "happy" in text.lower() or "good" in text.lower():
-        return "positive"
+        return "positif"
     if "sad" in text.lower() or "bad" in text.lower():
         return "negative"
     return "neutral"
